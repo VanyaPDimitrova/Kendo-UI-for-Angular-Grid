9ae68f40104747b54ae29d9a3a6b91f0014f8369
Vanya Dimitrova <vapedim@yahoo.com>
2023-04-04 14:36:11 +0300

    - add license - add Kendo UI Grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
 import { Component } from '@angular/core';
+import { ProductService } from "./product.service";
+import { products }from "./data.products";
+import { PageChangeEvent } from '@progress/kendo-angular-grid';
+import { DataResult, SortDescriptor } from '@progress/kendo-data-query';
+import { Observable } from 'rxjs';
+
+
+type GridDataResult = DataResult;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ProductService]
 })
+
 export class AppComponent {
   title = 'appName';
+  
+  public gridData: any = products;
+
+  constructor(private service: ProductService) {
+    this.loadGridItems();
+  }
+  public gridItems: Observable<GridDataResult>;
+
+  public pageSize: number = 10;
+  public skip: number = 0;
+  public sortDescriptor: SortDescriptor[] = [];
+  public filterTerm: number = 0;
+
+  public pageChange(event: PageChangeEvent): void {
+      this.skip = event.skip;
+      this.loadGridItems();
+  }
+
+  public handleSortChange(descriptor: SortDescriptor[]): void {
+      this.sortDescriptor = descriptor;
+      this.loadGridItems();
+  }
+
+  private loadGridItems(): void {
+      this.gridItems = this.service.getProducts(
+        this.skip,
+        this.pageSize,
+        this.sortDescriptor,
+        this.filterTerm
+      );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DateInputsModule } from '@progress/kendo-angular-dateinputs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DialogsModule } from '@progress/kendo-angular-dialog';
+import { GridModule } from '@progress/kendo-angular-grid';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DateInputsModule,
+    BrowserAnimationsModule,
+    DialogsModule,
+    GridModule
   ],
   providers: [],
   bootstrap: [AppComponent]
